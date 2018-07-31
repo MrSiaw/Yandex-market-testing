08c62e495ff2ae67eecd0c0e0cd32379e9929784
DATATEH\student <barselont@gmail.com>
2018-07-31 18:08:11 +0500

    Сортировка по цене, выбор 1ого товара и нажатие на него

--- src/test/java/cheepItem.java
+++ src/test/java/cheepItem.java
@@ -19,6 +19,20 @@ public class cheepItem {
         searchField.sendKeys("Honor 9");
         WebElement button = driver.findElement(By.xpath("//button[@type='submit']"));
         button.click();
+        WebElement sort = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[1]/div[3]/a[1]"));
+        driver.manage().timeouts().implicitlyWait(8, SECONDS);
+        sort.click();
+        SECONDS.sleep(5);
+        WebElement firstResult = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[4]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[2]/a[1]"));
+        firstResult.click();
+        driver.manage().timeouts().implicitlyWait(30, SECONDS);
+        String price = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[4]/div[4]/div[3]/div[1]/div[1]/div[1]/div[2]/span[1]")).getText();
+        String seller = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[4]/div[4]/div[3]/div[1]/div[1]/div[1]/div[5]/div[1]/div[1]/span[1]/a[1]")).getText();
+        System.out.println("ATTENTION");
+        System.out.println(price+" "+seller);
+        System.out.println("ATTENTION");
+
+        driver.manage().timeouts().implicitlyWait(8, SECONDS);
         driver.quit();
     }
 }
