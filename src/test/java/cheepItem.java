4fb88497d4c1e38e49b85e7e14d3371fb27b3529
DATATEH\student <barselont@gmail.com>
2018-07-31 18:06:51 +0500

    Ввод в поиск запроса и нажатие на кнопку "Поиск"

--- src/test/java/cheepItem.java
+++ src/test/java/cheepItem.java
@@ -1,3 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 public class cheepItem {
-    
+    @Test
+    public void opensite() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Student/Downloads/chromedriver_win32/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://market.yandex.ru/");
+        driver.manage().timeouts().implicitlyWait(30, SECONDS);
+        WebElement searchField = driver.findElement(By.xpath("//input[@id='header-search']"));
+        searchField.clear();
+        searchField.sendKeys("Honor 9");
+        WebElement button = driver.findElement(By.xpath("//button[@type='submit']"));
+        button.click();
+        driver.quit();
+    }
 }
