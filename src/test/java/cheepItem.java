acf75a4630bfe16e343ba5df9258cc02df1727ba
DATATEH\student <barselont@gmail.com>
2018-08-02 16:20:10 +0500

    Добавление зависимостей и плагина Allure для формирования отчета, разбиение теста на шаги, добавление метода для создания скриншотов. Для просмотра отчета использовать команду: allure serve allure-results

--- src/test/java/cheepItem.java
+++ src/test/java/cheepItem.java
@@ -1,58 +1,98 @@
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
 import static java.util.concurrent.TimeUnit.SECONDS;
 
+
 public class cheepItem {
-    @Test
-    public void opensite() throws InterruptedException {
-        ArrayList<Item> results = new ArrayList<Item>();
-        int resultscontains=0;
+    @Test(description = "Search option test")
+    public void findCheepItem() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:/Users/Student/Downloads/chromedriver_win32/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        open(driver);
+        sendsearchfield(driver);
+        submitsearch(driver);
+        getresult(driver);
+        sortclick(driver);
+        firstresultclick(driver);
+        getpriceandseller(driver);
+    }
+    @Step("Open site ")
+    public void open(WebDriver driver){
         driver.manage().window().maximize();
         driver.get("https://market.yandex.ru/");
-        driver.manage().timeouts().implicitlyWait(30, SECONDS);
+        saveAllureScreenshot(driver);
+    }
+    @Step("Input into search field")
+    public void sendsearchfield(WebDriver driver){
         WebElement searchField = driver.findElement(By.xpath("//input[@id='header-search']"));
         searchField.clear();
         searchField.sendKeys("Honor 9");
+        saveAllureScreenshot(driver);
+    }
+    @Step("Click on search button")
+    public void submitsearch(WebDriver driver){
         WebElement button = driver.findElement(By.xpath("//button[@type='submit']"));
         button.click();
-        driver.manage().timeouts().implicitlyWait(8, SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10,SECONDS);
+        saveAllureScreenshot(driver);
+    }
+    @Step("Get list of search results")
+    public void getresult(WebDriver driver){
+        ArrayList<Item> results = new ArrayList<Item>();
+        int resultscontains=0;
         List<WebElement> titles = driver.findElements(By.xpath("//div[@class='n-snippet-cell2__title']"));
         List<WebElement> prices = driver.findElements(By.xpath("//div[@class='n-snippet-cell2__main-price']"));
         for (int i=0;i<(prices.size());i=i+1){
             Item phone = new Item();
-            phone.name=prices.get(i).getText();
-            phone.price=titles.get(i).getText();
+            phone.setItem(prices.get(i).getText(),titles.get(i).getText());
             if (phone.price.contains("Honor 9")){
                 resultscontains=1;
             }
             results.add(i,phone);
         }
-        if (resultscontains==1)
-            {
-                WebElement sort = driver.findElement(By.xpath("//div[@class='n-filter-sorter i-bem n-filter-sorter_js_inited']//a"));
-                sort.click();
-                SECONDS.sleep(5);
-                WebElement firstResult = driver.findElement(By.xpath("//div[@class='n-snippet-cell2__title']//a"));
-                firstResult.click();
-                driver.manage().timeouts().implicitlyWait(30, SECONDS);
-                String price = driver.findElement(By.xpath("//div[@class='n-product-price-cpa2']")).getText();
-                String seller = driver.findElement(By.xpath("//div[@class='n-product-default-offer-multiple__shop']//a")).getText();
-                System.out.println("Seller: "+seller+"; Price: "+price+".");
-            }
-        else
-            {
-                System.out.println("No item in search results");
-            }
+    }
+    @Step("Sort by price")
+    public void sortclick(WebDriver driver) throws InterruptedException {
+        WebElement sort = driver.findElement(By.xpath("//div[@class='n-filter-sorter i-bem n-filter-sorter_js_inited']//a"));
+        sort.click();
+        SECONDS.sleep(3);
+        saveAllureScreenshot(driver);
+        //driver.manage().timeouts().pageLoadTimeout(10,SECONDS);
+
+    }
+    @Step("Click on first item in results")
+    public void firstresultclick(WebDriver driver) {
+        WebElement firstResult = driver.findElement(By.xpath("//div[@class='n-snippet-cell2__title']//a"));
+        firstResult.click();
+        driver.manage().timeouts().pageLoadTimeout(10, SECONDS);
+        saveAllureScreenshot(driver);
+    }
+    @Step("Get price and name seller and quit browser")
+    public  void getpriceandseller(WebDriver driver) {
+        String price = driver.findElement(By.xpath("//div[@class='n-product-price-cpa2']")).getText();
+        String seller = driver.findElement(By.xpath("//div[@class='n-product-default-offer-multiple__shop']//a")).getText();
+        System.out.println("Seller: " + seller + "; Price: " + price + ".");
+        Allure.addAttachment("Search results:", "text/plain","Seller: " + seller + "; Price: " + price + "." );
         driver.quit();
     }
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveAllureScreenshot( WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+    /*
+    @Attachment(value = "Cheep item", type = "text")
+    public static String getnameandprice (String name,String price) throws IOException {
+        //return Files.readAllBytes());
+    }*/
 }
