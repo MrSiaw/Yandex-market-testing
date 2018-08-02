4e0f8ed539a09e9e6e3513a9df6912d96b6b697f
DATATEH\student <barselont@gmail.com>
2018-08-02 17:46:52 +0500

    Добавление зависимостей для javadoc

--- src/test/java/Item.java
+++ src/test/java/Item.java
@@ -1,9 +1,9 @@
 public class Item {
         String name;
         String price;
-        public void setItem(String namefromsite,String pricefromsite){
-            Item.this.name=namefromsite;
-            Item.this.price= pricefromsite;
+        public void setItem(String namefromsite,String pricefromsite) {
+            Item.this.name = namefromsite;
+            Item.this.price = pricefromsite;
         }
 
 }
--- src/test/java/cheepItem.java
+++ src/test/java/cheepItem.java
@@ -51,16 +51,20 @@ public class cheepItem {
     public void getresult(WebDriver driver){
         ArrayList<Item> results = new ArrayList<Item>();
         int resultscontains=0;
+        Item phone = new Item();
         List<WebElement> titles = driver.findElements(By.xpath("//div[@class='n-snippet-cell2__title']"));
         List<WebElement> prices = driver.findElements(By.xpath("//div[@class='n-snippet-cell2__main-price']"));
         for (int i=0;i<(prices.size());i=i+1){
-            Item phone = new Item();
-            phone.setItem(prices.get(i).getText(),titles.get(i).getText());
+
+            phone.setItem(titles.get(i).getText(),prices.get(i).getText());
             if (phone.price.contains("Honor 9")){
                 resultscontains=1;
             }
             results.add(i,phone);
+            System.out.println(phone.name+" "+phone.price);
         }
+        listofitems(results);
+        //Allure.addAttachment("Search results:", "text/plain", toString(results));
     }
     @Step("Sort by price")
     public void sortclick(WebDriver driver) throws InterruptedException {
@@ -69,7 +73,6 @@ public class cheepItem {
         SECONDS.sleep(3);
         saveAllureScreenshot(driver);
         //driver.manage().timeouts().pageLoadTimeout(10,SECONDS);
-
     }
     @Step("Click on first item in results")
     public void firstresultclick(WebDriver driver) {
@@ -90,9 +93,18 @@ public class cheepItem {
     public byte[] saveAllureScreenshot( WebDriver driver) {
         return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
-    /*
-    @Attachment(value = "Cheep item", type = "text")
-    public static String getnameandprice (String name,String price) throws IOException {
-        //return Files.readAllBytes());
-    }*/
+    @Attachment(value = "List of Items", type = "text")
+    public  ArrayList<String> listofitems (ArrayList<Item> Items) {
+        Item phone = new Item();
+        String info="";
+        ArrayList<String> ItemsInfo = new ArrayList<String>();
+        for (int i=0;i<Items.size();i=i+1)
+        {
+            phone = Items.get(i);
+            info="Name: "+phone.name+" Price: "+phone.price;
+            ItemsInfo.add(i,info);
+            info="";
+        }
+        return ItemsInfo;
+    }
 }
